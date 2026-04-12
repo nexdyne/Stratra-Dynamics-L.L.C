a55fd7f183d38fd4017fcae179b6ecbe1407bc2b
nexdyne <nexdyne.team@gmail.com>
2026-04-11 23:21:11 -0400

    Remove icons from service cards
    Removed the icon container from the HomePage component.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -380,11 +380,7 @@ const HomePage = () => {
                       className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-700"
                     />
                     <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent"></div>
-                    <div className="absolute bottom-4 left-4">
-                      <div className="bg-orange-500 text-white p-2 rounded">
-                        <Icon className="w-5 h-5" />
-                      </div>
-                    </div>
+
                   </div>
 
                   {/* Content */}
